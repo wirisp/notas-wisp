6a54722aa30b28c4ba649bdab55b7269d4b4f926
abdias <abdias@alumnos.uaslp.edu.mx>
2023-04-13 19:47:31 -0600

    Update radacct_trim.sh

--- radacct_trim.sh
+++ radacct_trim.sh
@@ -2,20 +2,13 @@
 ####!/bin/sh
 #set -x
 #MYSQL DETAILS
-DATE=`date`
-logger radacct_trim script started $DATE
 SQLUSER="root"
 SQLPASS="YOUR_PASSWORD"
 DB="radius"
-TBL_ARCH="radacct_archive"
-TBL_ARCH_EXISTS=$(printf 'SHOW TABLES LIKE "%s"' "$TBL_ARCH")
 Days="20"
 export MYSQL_PWD=$SQLPASS
 CMD="mysql -u$SQLUSER --skip-column-names -s -e"
 # This is one time step.
-echo "
-Script Started @ $DATE
-"
 echo "- Step 1 : Checking for DB: $DB / TABLE: $TBL_ARCH ..."
 DBCHK=`mysqlshow --user=$SQLUSER $DB | grep -v Wildcard | grep -o $DB`
 if [ "$DBCHK" == "$DB" ]; then
@@ -24,25 +17,9 @@ else
 echo " > $DB not found. Creating now ..."
 $CMD "create database if not exists $DB;"
 fi
-if [[ $(mysql -u$SQLUSER -e "$TBL_ARCH_EXISTS" $DB) ]]
-then
-echo " > $TBL_ARCH TABLE found IN DB: $DB"
-else
-echo " > $TBL_ARCH TABLE not found IN DB: $DB / Creating now ..."
-$CMD "use $DB; create table if not exists $TBL_ARCH LIKE radacct;"
-fi
  
 # Start Action: copy data from radacct to new db/archive table
-NOTULL_COUNT=`$CMD "use $DB; select count(*) from radacct WHERE acctstoptime <= DATE_SUB(CURDATE(), INTERVAL $Days day);"`
-echo "- Step 2 : Found $NOTULL_COUNT records in radacct table , Now copying $NOTULL_COUNT records to $TBL_ARCH table ..."
-$CMD "use $DB; INSERT IGNORE INTO $TBL_ARCH SELECT * FROM radacct WHERE acctstoptime <= DATE_SUB(CURDATE(), INTERVAL $Days day);"
-echo "- Step 3 : Deleting $NOTULL_COUNT records old data from radacct table (which have acctstoptime NOT NULL) ..."
-# --- Now Delete data from CURRENT RADACCT table so that it should remain fit and smart ins size
-$CMD "use $DB; DELETE FROM radacct WHERE date(acctstoptime) < (CURDATE() - INTERVAL $Days day);"
-echo "- Step 4 : Copying old data from $TBL_ARCH older then $Days day ..."
-# --- Now Delete data from RADACCT_ARCHIVE table so that it should not grow either more than we required i.e 1 Year - one year archived data is enough IMO
-$CMD "use $DB; DELETE FROM $TBL_ARCH WHERE date(acctstarttime) < (CURDATE() - INTERVAL $Days day);"
-DATE=`date`
-logger radacct_trim script ended with $NOTULL_COUNT records processed for trimming @ $DATE
-echo "
-radacct_trim script ended with $NOTULL_COUNT records processed for trimming @ $DATE"
+NOTULL_COUNT=`$CMD "use $DB; select count(*) from radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL $Days day);"`
+echo "- Step 3 : Eliminando $NOTULL_COUNT entradas de la tabla radacct (Fecha posterior a $Days) ..."
+$CMD "use $DB; DELETE FROM radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL $Days day);"
+echo "Finalizada la limpieza de la tabla raddact"
