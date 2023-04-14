51017c13c955862e9d67ef092e863e43c289e3b3
abdias <abdias@alumnos.uaslp.edu.mx>
2023-04-13 19:52:28 -0600

    Update radacct_trim.sh

--- radacct_trim.sh
+++ radacct_trim.sh
@@ -9,17 +9,16 @@ Days="20"
 export MYSQL_PWD=$SQLPASS
 CMD="mysql -u$SQLUSER --skip-column-names -s -e"
 # This is one time step.
-echo "- Step 1 : Checking for DB: $DB / TABLE: $TBL_ARCH ..."
+echo "- Paso 1 : Checando existencia de la DB: $DB /.."
 DBCHK=`mysqlshow --user=$SQLUSER $DB | grep -v Wildcard | grep -o $DB`
 if [ "$DBCHK" == "$DB" ]; then
-echo " > $DB DB found"
+echo " > $DB DB encontrada"
 else
-echo " > $DB not found. Creating now ..."
-$CMD "create database if not exists $DB;"
+echo " > $DB no encontrada ..."
 fi
  
 # Start Action: copy data from radacct to new db/archive table
 NOTULL_COUNT=`$CMD "use $DB; select count(*) from radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL $Days day);"`
-echo "- Step 3 : Eliminando $NOTULL_COUNT entradas de la tabla radacct (Fecha posterior a $Days) ..."
+echo "- Paso 2 : Eliminando $NOTULL_COUNT entradas de la tabla radacct (Fecha posterior a $Days dias) ..."
 $CMD "use $DB; DELETE FROM radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL $Days day);"
 echo "Finalizada la limpieza de la tabla raddact"
