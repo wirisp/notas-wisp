8313e9c3722e04524b1675245e4635bd792c137b
abdias <abdias@alumnos.uaslp.edu.mx>
2023-04-22 15:10:27 -0600

    Update Expirados-freeradius.sh

--- Expirados-freeradius.sh
+++ Expirados-freeradius.sh
@@ -2,14 +2,14 @@
 #set -x
 # This script delets users who have expired 2 months ago. and then delete there records from all tables.
 # Syed Jahanzaib / June 2019
-SQLPASS="niq@84U"
+SQLPASS="84Uniq@"
 export MYSQL_PWD=$SQLPASS
 > /tmp/expired.users.txt
 
 #mysql -uroot -e “use radius; select username from rm_users where expiration BETWEEN ‘2010-01-01’ AND ‘2019-04-30’;” |sort > /tmp/expired.users.txt
 
 # Fetch users who have expired 2 months ago & before, (using expired date), BE CAREFUL WHEN USING THIS
-mysql -uroot -e "use radius; SELECT username FROM usadas WHERE ant <= DATE_SUB(CURDATE(), INTERVAL 10 day) and (groupname = '2hPausada' OR groupname = '12hPausada')" |sort > /tmp/expired.users.txt
+mysql -uroot -e "use radius; SELECT username FROM usadas WHERE min <= DATE_SUB(CURDATE(), INTERVAL 10 day) and (groupname = '2hPausada' OR groupname = '12hPausada')" |sort > /tmp/expired.users.txt
 num=0
 cat /tmp/expired.users.txt | while read users
 do
@@ -23,5 +23,11 @@ mysql -uroot -e "use radius; DELETE FROM radusergroup WHERE username = '$USERNAM
 mysql -uroot -e "use radius; DELETE FROM userbillinfo WHERE username = '$USERNAME';"
 
 done
-#Copia usuarios a root
-cp /tmp/expired.users.txt /root/exp.txt
+##Copia usuarios a root
+cp /tmp/expired.users.txt /root/scripts/exp.txt
+## Parte 2 para limpiar la base de datos en radpostauth && raddact
+#mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
+#mysql -uroot -e "use radius; DELETE FROM radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
+#mysql -uroot -e "use radius; DELETE FROM userbillinfo WHERE creationdate <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
+#mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
+#echo "Base de datos limpiada correctamente"
