475ee8e2daecfee3bedeac62ce963f44d36c2281
abdias <abdias@alumnos.uaslp.edu.mx>
2023-08-28 06:14:47 -0600

    Update eliminabatchdalo.sh

--- eliminabatchdalo.sh
+++ eliminabatchdalo.sh
@@ -2,7 +2,7 @@
 #set -x
 # Este script elimina fichas de hace 10 dias de uso, y tambien limpia las bases de datos de hace 30 dias.
 # Cambiar el password de tu DB
-SQLPASS="85Uniq@"
+SQLPASS="Uniq@"
 export MYSQL_PWD=$SQLPASS
 > /tmp/expired.users.txt
 
